10bfa46e383e1a0bf1233ff4d1efb649409fe3ad
joaovictorruivo <joao4920oliveira@gmail.com>
2026-01-27 20:33:10 -0300

    Adicionado enuncioado do Ex002 de _7Tratamentos...

--- ExerciciosPooCsharp/_7TratamentoExcecoes/Ex002/Program72.cs
+++ ExerciciosPooCsharp/_7TratamentoExcecoes/Ex002/Program72.cs
@@ -1,9 +1,44 @@
-﻿using ExerciciosPooCsharp._7TratamentoExcecoes.Ex002.Entites.Exceptions;
-using ExerciciosPooCsharp._7TratamentoExcecoes.Ex002.Entites;
+﻿using ExerciciosPooCsharp._7TratamentoExcecoes.Ex002.Entites;
 using System;
-using System.Collections.Generic;
+using ExerciciosPooCsharp._7TratamentoExcecoes.Ex002.Entites.Exceptions;
+using System.Diagnostics.Metrics;
 using System.Globalization;
-using System.Text;
+using System.Reflection.Metadata;
+
+/*Fazer um programa para ler os dados de uma conta bancária e depois realizar um
+saque nesta conta bancária, mostrando o novo saldo. Um saque não pode ocorrer
+ou se não houver saldo na conta, ou se o valor do saque for superior ao limite de
+saque da conta
+
+Enter account data
+Number: 8021
+Holder: Bob Brown
+Initial balance: 500.00
+Withdraw limit: 300.00
+Enter amount for withdraw: 100.00
+New balance: 400.00
+Enter account data
+Number: 8021
+Holder: Bob Brown
+Initial balance: 500.00
+Withdraw limit: 300.00
+Enter amount for withdraw: 400.00
+Withdraw error: The amount exceeds withdraw limit
+
+Enter account data
+Number: 8021
+Holder: Bob Brown
+Initial balance: 500.00
+Withdraw limit: 300.00
+Enter amount for withdraw: 800.00
+Withdraw error: The amount exceeds withdraw limit
+Enter account data
+Number: 8021
+Holder: Bob Brown
+Initial balance: 200.00
+Withdraw limit: 300.00
+Enter amount for withdraw: 250.00
+Withdraw error: Not enough balance*/
 
 namespace ExerciciosPooCsharp._7TratamentoExcecoes.Ex002
 {
