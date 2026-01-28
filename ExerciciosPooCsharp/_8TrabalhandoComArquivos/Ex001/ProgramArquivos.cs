669d7d1080297e5c8a5562231ffa32f85e4b0546
joaovictorruivo <joao4920oliveira@gmail.com>
2026-01-27 21:18:44 -0300

    Adicionado _8...\Ex001 sobre arquivos, Program não executavel. Materias: Trabalhando com arquivos.

---
+++ ExerciciosPooCsharp/_8TrabalhandoComArquivos/Ex001/ProgramArquivos.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using ExerciciosPooCsharp._8TrabalhandoComArquivos.Ex001.Entites;
+/*Fazer um programa para ler o caminho de um arquivo .csv
+contendo os dados de itens vendidos. Cada item possui um
+nome, preço unitário e quantidade, separados por vírgula. Você
+deve gerar um novo arquivo chamado "summary.csv", localizado
+em uma subpasta chamada "out" a partir da pasta original do
+arquivo de origem, contendo apenas o nome e o valor total para
+aquele item (preço unitário multiplicado pela quantidade)
+
+Example:
+
+Source file:
+TV LED,1290.99,1
+Video Game Chair,350.50,3
+Iphone X,900.00,2
+Samsung Galaxy 9,850.00,2
+
+Output file (out/summary.csv):
+TV LED,1290.99
+Video Game Chair,1051.50
+Iphone X,1800.00
+Samsung Galaxy 9,1700.00*/
+
+namespace ExerciciosPooCsharp._8TrabalhandoComArquivos.Ex001
+{
+    internal class ProgramArquivos
+    {
+        public static void Executar()
+        {
+            Console.Write("Enter file full path: ");
+            string sourceFilePath = Console.ReadLine();
+
+            try
+            {
+                string[] lines = File.ReadAllLines(sourceFilePath);
+
+                string sourceFolderPath = Path.GetDirectoryName(sourceFilePath);
+                string targetFolderPath = sourceFolderPath + @"\out";
+                string targetFilePath = targetFolderPath + @"\summary.csv";
+
+                Directory.CreateDirectory(targetFolderPath);
+
+                using (StreamWriter sw = File.AppendText(targetFilePath))
+                {
+                    foreach (string line in lines)
+                    {
+
+                        string[] fields = line.Split(',');
+                        string name = fields[0];
+                        double price = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        int quantity = int.Parse(fields[2]);
+
+                        Product prod = new Product(name, price, quantity);
+
+                        sw.WriteLine(prod.Name + "," + prod.Total().ToString("F2", CultureInfo.InvariantCulture));
+                    }
+                }
+
+            }
+            catch (IOException e)
+            {
+                Console.WriteLine("");
+            }
+
+        }
+    }
+}
