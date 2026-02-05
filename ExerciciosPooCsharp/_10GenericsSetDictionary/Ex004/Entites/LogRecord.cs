10ed9e71469700e3b97da32cefa63777771e4a9b
joaovictorruivo <joao4920oliveira@gmail.com>
2026-02-04 23:27:25 -0300

    Add Ex004 referente a Set _10...

---
+++ ExerciciosPooCsharp/_10GenericsSetDictionary/Ex004/Entites/LogRecord.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+/*Um site de internet registra um log de acessos dos usuários. Um
+registro de log consiste no nome de usuário e o instante em que o
+usuário acessou o site no padrão ISO 8601, separados por espaço,
+conforme exemplo. Fazer um programa que leia o log de acessos a
+partir de um arquivo, e daí informe quantos usuários distintos
+acessaram o site.*/
+
+/*amanda 2020-08-26T20:45:08
+alex86 2020-08-26T21:49:37
+bobbrown 2020-08-27T03:19:13
+amanda 2020-08-27T08:11:00
+jeniffer3 2020-08-27T09:19:24
+alex86 2020-08-27T22:39:52
+amanda 2020-08-28T07:42:19*/
+
+//Enter file full path: c:\temp\in.txt
+//Total users: 4
+
+namespace ExerciciosPooCsharp._10GenericsSetDictionary.Ex004.Entites
+{
+    internal class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instant { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = obj as LogRecord;
+            return Username.Equals(other.Username);
+        }
+    }
+}
