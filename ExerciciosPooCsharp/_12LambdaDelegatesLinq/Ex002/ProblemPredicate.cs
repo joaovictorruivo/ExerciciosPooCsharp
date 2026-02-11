ae7daf402952ce76e6e7d65242b96817c4f03186
joaovictorruivo <joao4920oliveira@gmail.com>
2026-02-10 23:19:00 -0300

    Add Ex002 Predicate exemplo com RemoveAll.

---
+++ ExerciciosPooCsharp/_12LambdaDelegatesLinq/Ex002/ProblemPredicate.cs
@@ -0,0 +1,23 @@
+﻿using ExerciciosPooCsharp._12LambdaDelegatesLinq.Ex002.Entites;
+// Exemplo com RemoveAll
+namespace ExerciciosPooCsharp._12LambdaDelegatesLinq.Ex002
+{
+    internal class ProblemPredicate
+    {
+        public static void Executar()
+        {
+            List<Product> list = new List<Product>();
+
+            list.Add(new Product("Tv", 900.00));
+            list.Add(new Product("Mouse", 50.00));
+            list.Add(new Product("Tablet", 350.50));
+            list.Add(new Product("HD Case", 80.90));
+
+            list.RemoveAll(p => p.Price >= 100.0);
+            foreach (Product p in list)
+            {
+                Console.WriteLine(p);
+            }
+        }    
+    }
+}
