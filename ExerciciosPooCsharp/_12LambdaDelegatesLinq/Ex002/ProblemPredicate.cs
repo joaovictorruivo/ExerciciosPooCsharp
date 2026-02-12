3874b8e32f8e1758385d2defa2000a2db123c35b
joaovictorruivo <joao4920oliveira@gmail.com>
2026-02-11 22:57:20 -0300

    Usando expressão LAMBDA Inline

--- ExerciciosPooCsharp/_12LambdaDelegatesLinq/Ex002/ProblemPredicate.cs
+++ ExerciciosPooCsharp/_12LambdaDelegatesLinq/Ex002/ProblemPredicate.cs
@@ -16,9 +16,9 @@ namespace ExerciciosPooCsharp._12LambdaDelegatesLinq.Ex002
             list.Add(new Product("Tablet", 350.50));
             list.Add(new Product("HD Case", 80.90));
 
-            Func<Product, string> func = p => p.Name.ToUpper();//se usar {chaves}, obrigado colocar a palavra return
+            //Func<Product, string> func = p => p.Name.ToUpper();//se usar {chaves}, obrigado colocar a palavra return
             
-            List<string> resulte = list.Select(func).ToList();
+            List<string> resulte = list.Select(p => p.Name.ToUpper()).ToList(); //Usando LAMBDA Inline
             foreach(string s in resulte)
             {
                 Console.WriteLine(s);
