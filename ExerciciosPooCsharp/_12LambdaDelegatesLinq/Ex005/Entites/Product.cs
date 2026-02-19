7dae5372005ff361e2741400b8a0b942b6621ae8
joaovictorruivo <joao4920oliveira@gmail.com>
2026-02-19 16:19:58 -0300

    add ex005 resolvido. _12...

---
+++ ExerciciosPooCsharp/_12LambdaDelegatesLinq/Ex005/Entites/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace ExerciciosPooCsharp._12LambdaDelegatesLinq.Ex005.Entites
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public Product(string name, double price)
+        {
+            Name = name;
+            Price = price;
+        }
+    }
+}
