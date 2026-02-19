08528d999b51437030b6426acd627c438ab5c7df
joaovictorruivo <joao4920oliveira@gmail.com>
2026-02-19 19:07:48 -0300

    adicionando enunciado do exercicio.

--- ExerciciosPooCsharp/_12LambdaDelegatesLinq/Ex005/ExercReso.cs
+++ ExerciciosPooCsharp/_12LambdaDelegatesLinq/Ex005/ExercReso.cs
@@ -4,6 +4,29 @@ using System.IO;
 using System.Linq;
 using System.Globalization;
 using ExerciciosPooCsharp._12LambdaDelegatesLinq.Ex005.Entites;
+/*Fazer um programa para ler um conjunto de produtos a partir de um
+arquivo em formato .csv (suponha que exista pelo menos um produto).
+Em seguida mostrar o preço médio dos produtos. Depois, mostrar os
+nomes, em ordem decrescente, dos produtos que possuem preço
+inferior ao preço médio.
+
+ Input file:
+Tv,900.00
+Mouse,50.00
+Tablet,350.50
+HD Case,80.90
+Computer,850.00
+Monitor,290.00
+Execution:
+Enter full file path: c:\temp\in.txt
+Average price: 420.23
+Tablet
+Mouse
+Monitor
+HD Case
+
+ */
+
 namespace ExerciciosPooCsharp._12LambdaDelegatesLinq.Ex005
 {
     internal class ExercReso
