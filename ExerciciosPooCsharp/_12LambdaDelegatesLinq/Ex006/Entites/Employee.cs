c23a0c093951f86a08e9caa80c7ca2aa980da0aa
joaovictorruivo <joao4920oliveira@gmail.com>
2026-02-20 09:38:49 -0300

    Add Ex006 _12 exercicio fixação lambda.

---
+++ ExerciciosPooCsharp/_12LambdaDelegatesLinq/Ex006/Entites/Employee.cs
@@ -0,0 +1,16 @@
+﻿namespace ExerciciosPooCsharp._12LambdaDelegatesLinq.Ex006.Entites
+{
+    internal class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public double Salary { get; set; }
+
+        public Employee(string name, string email, double salary)
+        {
+            Name = name;
+            Email = email;
+            Salary = salary;
+        }
+    }
+}
